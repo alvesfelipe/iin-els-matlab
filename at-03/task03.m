7dd3e3be50855a9232881e358ad835a14bc726ce
Felipe Alves Araújo <felipealves@lavid.ufpb.br>
2017-08-27 14:45:59 -0300

    Update the range of the vector and type of its elements

--- at-03/task03.m
+++ at-03/task03.m
@@ -3,8 +3,9 @@
 % This task implements an example of conditional commands (if, else,
 % elseif) and looping (for).
 
-% Create a vector of 1000 elements
-v = randi([-5,5],5,1);
+% Create a vector of 1000 elements using randn function as specified by 
+% professor
+v = randn(1000,1);
 
 % Create a cell for store informations about the vector 'v' elements
 c = cell(4, 1);
@@ -16,7 +17,7 @@ for i = 1:size(v)
        c{1} = [c{1}; v(i)];
    % stores odd numbers in the second position of the cell
    elseif mod(v(i), 2) == 1
-       c{2} = [c{2}; v(i)];      
+       c{2} = [c{2}; v(i)];
    end
    
    % stores positive numbers in the third position of the cell
