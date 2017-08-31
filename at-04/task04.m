8e099bee775ad27bbc8e5d08b538d3d2e73fadde
Felipe Alves Araújo <felipealves@lavid.ufpb.br>
2017-08-30 23:05:59 -0300

    Fix task, search by repeated names

--- at-04/task04.m
+++ at-04/task04.m
@@ -6,12 +6,20 @@
 data = load('Atividade4.mat');
 
 S = [];
-aux = [];
+aux = cell(1, size(data.alunos, 1));
 
 % gets all students, splits by blank space and adds in a structs
 for i = 1:size(data.alunos, 1)
-    aux = strsplit(data.alunos{i}, ' ');
-    aux{1} = clear_str({aux{1}});
-    S.(char(aux{1})) = struct('nome', {data.alunos{i}}, 'matricula',...
-                              {data.matricula{i}});
+    aux{i} = cell(strsplit(data.alunos{i}, ' '));
+    aux{i}{1} = clear_str({aux{i}{1}});
+    % search by repeated names
+    for j = 1:i-1
+        % edit repeated names before put in the struct
+        if strcmp(aux{i}{1}, aux{j}{1})
+            aux{i}{1} = strcat(aux{i}{1}, num2str(i));
+        end
+        S.(char(aux{i}{1})) = struct('nome', {data.alunos{i}}, 'matricula',...
+                          {data.matricula{i}});
+    end
 end
+clear aux i j data
